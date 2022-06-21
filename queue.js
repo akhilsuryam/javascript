31ccecffcb74e68a0fe98cb0a9dde52ef835a3e3
akhilsuryam <99366510+akhilsuryam@users.noreply.github.com>
2022-06-21 12:24:20 +0530

    Add files via upload

---
+++ queue.js
@@ -0,0 +1,45 @@
+class node{
+    constructor(data){
+        this.value = data
+        this.next = null;         
+    }
+}
+
+class Queue{
+    constructor(){
+        this.first= null;
+        this.last = null;
+        this.length = 0;
+
+    }
+
+    Enqueue(data){
+        const newNode = new node(data);
+        if(this.length ==0){
+            this.first = newNode;
+            this.last = newNode;
+            this.length++
+        }
+        else{
+            this.last.next = newNode;
+            this.last = newNode
+            this.length++
+        }
+    }
+
+    display(){
+        console.log(this.first);
+        console.log(this.last); 
+    }
+    dequeue(){
+        this.first = this.first.next
+        this.length--;
+    }
+}
+
+const myQueue = new Queue()
+myQueue.Enqueue(1);
+myQueue.Enqueue(2);
+myQueue.Enqueue(3);
+myQueue.dequeue()
+myQueue.display()
